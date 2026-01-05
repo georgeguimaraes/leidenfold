70c980be5bc838e9f1f63fc12e650dda916a4e34
George Guimarães <george.guimaraes@gmail.com>
2026-01-05 09:30:22 -0300

    chore: Update checksums for v0.1.0 release

--- checksum-Elixir.Leidenfold.Native.exs
+++ checksum-Elixir.Leidenfold.Native.exs
@@ -1,4 +1,4 @@
 %{
-  "leidenfold_native-nif-2.16-aarch64-apple-darwin.tar.gz" => "sha256:PLACEHOLDER",
-  "leidenfold_native-nif-2.16-x86_64-unknown-linux-gnu.tar.gz" => "sha256:PLACEHOLDER"
+  "leidenfold_native-nif-2.16-aarch64-apple-darwin.tar.gz" => "sha256:8bd8805ca80eb6918b8196c9bc1663de8e3d0d7364d4697dc16a95b4298768c0",
+  "leidenfold_native-nif-2.16-x86_64-unknown-linux-gnu.tar.gz" => "sha256:6c3482429d4da3d363d7d4e1d747f5bad99213699def58be75545ea6e547ae68"
 }
